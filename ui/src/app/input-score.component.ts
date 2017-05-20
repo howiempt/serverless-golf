182205d9b209aac4eaa0228123dc90dd5f4b882f
Matt Howe <matthowe82@gmail.com>
2017-05-20 23:42:29 +0800

    improve score taking

--- ui/src/app/input-score.component.ts
+++ ui/src/app/input-score.component.ts
@@ -1,15 +1,25 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Output, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {GameService} from './services/game/game';
 
 @Component({
   selector: 'input-score',
   template: require('./input-score.component.html')
 })
-export class InputScoreComponent {
-  @Input() score: number;
+export class InputScoreComponent implements OnInit {
+  score: number;
+
   @Output() scoreSaved: EventEmitter<number> = new EventEmitter<number>();
-  constructor() {
+  constructor(private gameService: GameService) {
   }
+
+  ngOnInit() {
+    this.gameService.holeSelected$.subscribe(h => {
+      console.log('input-scores sub-h', h);
+      this.score = null;
+    });
+  }
+  
   saveScore() {
     console.log('saved');
     this.scoreSaved.next(this.score);
